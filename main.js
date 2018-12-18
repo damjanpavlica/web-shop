8f5c7b53e60b0f19e3fb7ec83288954673e12205
damjanpavlica <44097267+damjanpavlica@users.noreply.github.com>
2018-12-18 19:28:01 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+const ukupno = document.getElementById("ukupno")
+const cene = document.querySelectorAll(".cena")
+const kolicine = document.querySelectorAll("td:nth-child(4) input")
+
+function azuriraj() {
+  let total = 0
+  for (let i = 0; i < cene.length; i++) {
+    const cena = Number(cene[i].innerText)
+    const kolicina = Number(kolicine[i].value)
+    if (kolicina) total += cena * kolicina
+  }
+  ukupno.innerText = total
+}
+
+for (let i = 0; i < cene.length; i++) {
+  kolicine[i].addEventListener("input", azuriraj)
+}
+
+azuriraj()
