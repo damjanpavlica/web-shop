7389b3613543a54acb695aa67fbc5d410aff7c33
Damjan Pavlica <damjan.pavlica@code.edu.rs>
2018-12-22 11:03:31 +0100

    prosledjivanje poruke

--- js/main.js
+++ js/main.js
@@ -1,22 +1,29 @@
 const ukupnoElement = document.getElementById("ukupno")
 const brojArtikalaElement = document.getElementById("broj-artikala")
+const naziviElementi = document.querySelectorAll("td:nth-child(2)")
 const ceneElementi = document.querySelectorAll(".cena")
 const kolicineElementi = document.querySelectorAll("td:nth-child(4) input")
 
 function azuriraj() {
   let ukupno = 0
   let brojArtikala = 0
+  let poruka = "Narucili ste: \n"
   for (let i = 0; i < ceneElementi.length; i++) {
-    const cena = Number(ceneElementi[i].innerText)
     const kolicina = Number(kolicineElementi[i].value)
     if (kolicina) {
+      const naziv = naziviElementi[i].innerText
+      const cena = Number(ceneElementi[i].innerText)
       ukupno += cena * kolicina
       brojArtikala += kolicina
+      poruka += `- ${naziv} Komada: ${kolicina}, po ceni od ${cena} eura. \n`
     }
   }
-  ukupnoElement.innerText = ukupno
+  console.log(poruka);
   brojArtikalaElement.innerText = brojArtikala
-  localStorage.ukupno = ukupno
+  brojArtikalaElement.style.display = brojArtikala ? 'flex' : 'none'
+  ukupnoElement.innerText = ukupno.toFixed(2)
+  localStorage.ukupno = ukupno.toFixed(2)
+  localStorage.poruka = poruka
 }
 
 for (let i = 0; i < ceneElementi.length; i++) {
